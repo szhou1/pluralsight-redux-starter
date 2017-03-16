429936db9a11cece2fb9601b788e3c32c6390084
szhou1 <zhousteve3@gmail.com>
2017-03-16 14:51:28 -0700

    Add changes to redux

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight About page</h1>
+        <p></p>
+
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
